658b96aae0bd759193e5c369182bac1b508b6089
SimasBar1 <simasbartkevicius@gmail.com>
2024-12-03 18:39:02 +0200

    Update Main.java

--- Album/src/Main.java
+++ Album/src/Main.java
@@ -30,7 +30,7 @@ public class Main {
         LinkedList<Song> playList = new LinkedList<Song>();
         albums.get(0).addToPlayList("You can't do it right", playList);
         albums.get(0).addToPlayList("Holy man", playList);albums.get(0).addToPlayList("Speed king", playList);
-// Does not existalbums.get(0).addToPlayList(9, playList);
+        albums.get(0).addToPlayList(9, playList);
         albums.get(1).addToPlayList(3, playList);
         albums.get(1).addToPlayList(2, playList);
         albums.get(1).addToPlayList(24, playList);  // There is no track 24
